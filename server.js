6517cfc3b993c804f369dedc06b0163c302fd7df
Clyde Go <goclyde19@gmail.com>
2021-03-10 23:02:34 +0800

    unique alerts with users and alert time in minutes

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.DBURL;
 const mongo =  MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 let channel;
+let job = [];
 const tv = new TradingViewAPI();
 const client = new Client();
 const PREFIX = "$"
+const schedule = require('node-schedule');
 mongo.connect( err => {
     if(err) console.log(err);
     client.login(process.env.BOTTOKEN);
@@ -23,34 +25,31 @@ client.on('ready', async () => {
     channel = await client.channels.cache.find(channel => channel.id === process.env.CHANNELID);
 });
 
-async function PingMe(stocks, channel){
-    console.log(stocks);
+async function PingMe(stocks, channel, user){
     stocks.forEach(async (stock) => {
-        console.log(stock);
         let res = await scrapeStock(stock);
         if(res[0] === true) {
             console.log('alert triggered');
-            channel.send(`${stock.name} is below stop loss of ${stock.sl}. Current price is ${res[1]}`);
+            channel.send(`<@${user.id}> position ${stock.stockName} is below stop loss of ${stock.cutloss}. Current price is ${res[1]}`);
         }
     });
 }
 
 async function scrapeStock(stock){
     try {
-        let value = await tv.getTicker(stock.name);
-        
+        let value = await tv.getTicker(stock.stockName);
         if(value.lp === undefined) {
             return false;
         }
-        if(value.lp >= stock.sl){
-            console.log(`safe ${stock.name}: ${value.lp}`);
+        if(value.lp >= stock.cutloss){
+            console.log(`safe ${stock.stockName}: ${value.lp}`);
             return [false, value.lp];
         }
-        console.log(`cut ${stock.name}: ${value.lp}`);
+        console.log(`cut ${stock.stockName}: ${value.lp}`);
         return [true, value.lp];
     }
     catch(err){
-        console.log('error');
+        console.log(err);
         return false;
     }
 }
@@ -72,31 +71,97 @@ client.on('message', (message) => {
 
         if(command === 'add') {
             let temp = /^((\$add)(\ [A-Z]*\+([0-9]*)(\.([0-9]){1,4})?)*)$/.test(message.content);
-            console.log(temp);
             if(temp === false){
                 message.channel.send('Please follow add message format');
-                console.log('hit')
             }else {
                 addCommand(message, args);
             };
         }
 
-        if(command === 'delete'){
-            
+        if(command === 'clear'){
+            if(userExist(message.author.id)){
+                try{
+                    clearAlerts(message);
+                } catch(e){
+                    console.log(e);
+                }
+            } else {
+                message.channel.send("Please create stock alerts with $add");
+            }
+        }
+
+        if(command === 'start'){
+            sendAlerts(message);
+        }
+
+        if(command === 'stop'){
+            if(job[message.author.id] !== undefined){
+                job[message.author.id].cancel();
+                channel.send(`<@${message.author.id}> has ended alerts`);
+            } else {
+                channel.send(`<@${message.author.id}> no ongoing alerts`);
+            }
+        }
+
+        if(command === 'set'){
+            try {
+                setTimer(message, args[0]);
+            } catch(e) {
+                console.log(e);
+            }
         }
     }
 });
 
+async function setTimer(message, min){
+    let exist = await userExist(message.author.id);
+    if(exist) {
+        const collection = await mongo.db('stockAlerts').collection('Alerts');
+        collection.updateOne({userID: message.author.id}, {$set: {interval: min}}, function(err, res){
+            if(err) throw err;
+            channel.send(`<@${message.author.id}> has set alert interval to ${min}`);
+        });
+    } else {
+        channel.send(`<@${message.author.id}> please add an alert list first`);
+    }
+}
+
+async function sendAlerts(message){
+
+    let user = await getUser(message.author.id);
+    let stocks = user.stockAlerts;
+    if(stocks){
+        channel.send(`<@${message.author.id}> has started alerts interval of ${user.interval}`);
+        job[message.author.id] = schedule.scheduleJob(`${user.interval} * * * * *`, function(){
+            var current = new Date();
+            console.log(current);
+            PingMe(stocks, channel, message.author);
+        });
+    } else {
+        message.channel.send(`User has no stocks in alert list`);
+    }
+
+}
+
+async function clearAlerts(data){
+    const tempAlerts = null;
+    const collection = await mongo.db("stockAlerts").collection("Alerts");
+    collection.updateOne({userID: data.author.id}, {$set: {stockAlerts: tempAlerts}, function(err, res){
+        if(err) throw err;
+        console.log('cleared');
+        data.channel.send(`your alerts have been cleared`);
+    }});
+}
+
 async function addCommand(message, args){
-    console.log(args);
     let test = args[0].split('+');
-    console.log(test);
     let exist = await userExist(message.author.id);
 
     const data = {
         userID: message.author.id,
         username: message.author.username,
         usernumber: message.author.discriminator,
+        interval: '1',
         stockAlerts: [
         ]
     }
@@ -129,7 +194,6 @@ async function addCommand(message, args){
 }
 
 async function addNewUser(data){
-    console.log(data);
     const collection = await mongo.db("stockAlerts").collection("Alerts");
     collection.insertOne(data, function(err , res){
         if(err) throw err;
@@ -140,15 +204,11 @@ async function addNewUser(data){
 async function updateUserAlerts(data){
     const collection = await mongo.db("stockAlerts").collection("Alerts");
     let user = await getUser(data.userID);
-    console.log(user, 'user');
-    console.log(data, 'data');
-
     let tempAlerts = await removeDuplicates(user.stockAlerts, data.stockAlerts);
-
-    console.log(tempAlerts);
     collection.updateOne({userID: user.userID}, {$set : {stockAlerts: tempAlerts}}, function(err, res){
         if(err) throw err;
         console.log(`${data.username} has updated alert list`, tempAlerts);
+        channel.send(`<@${data.userID}> has updated alert list`);
     });
 }
 
@@ -183,7 +243,6 @@ async function removeDuplicates(currentData, newData) {
             tempStocks.push(el);
         }
     });
-    console.log(tempStocks);
     return tempStocks;
 }
 
